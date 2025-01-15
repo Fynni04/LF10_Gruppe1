c4379623d7bb3a6c3aec9ae57bcb1dd6409bf08e
Arne Paul Zillhardt <arne@zillhardt.com>
2025-01-15 13:11:30 +0100

    Keycloake YESSSSS BEABYYYYYYY

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,42 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {routes} from './app.routes';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {KeycloakBearerInterceptor, KeycloakService} from "keycloak-angular";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptorsFromDi())]
+  providers: [
+    provideRouter(routes),
+    KeycloakService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: KeycloakBearerInterceptor,
+      multi: true
+    },
+    provideHttpClient(withInterceptorsFromDi())]
 };
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'https://keycloak.szut.dev/auth',
+        realm: 'szut',
+        clientId: 'employee-management-service-frontend'
+      },
+      initOptions: {
+        checkLoginIframe: false,
+        onLoad: 'login-required', // 'login-required' , 'check-sso'
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      },
+      enableBearerInterceptor: true
+    });
+}
