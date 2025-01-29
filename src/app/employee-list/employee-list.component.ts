fa54b423febc274b87cde1af7482e67b4e14e745
RIS <f.risse@schierholz.de>
2025-01-29 09:34:31 +0100

    Add delete Employee in employee-list

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -4,10 +4,11 @@ import {Observable, of} from "rxjs";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Employee} from "../Employee";
 import Keycloak from "keycloak-js";
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-employee-list',
-    imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
     templateUrl: './employee-list.component.html',
     styleUrl: './employee-list.component.css'
 })
@@ -27,5 +28,28 @@ export class EmployeeListComponent {
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.keycloak.token}`)
     });
+  }
+  deletEmployee(employeeID:any){
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${this.keycloak.token}`);
+
+    this.http.delete<Employee>( 'http://localhost:8089/employees/'+employeeID, { headers } )
+      .subscribe
+      (
+        {
+          next: (response) =>
+          {
+            console.log('Employee deleted:', response);
+            alert('Mitarbeiter erfolgreich gelöscht!');
+
+          },
+          error: (error) =>
+          {
+            console.error('Error deleting employee:', error);
+            alert('Fehler beim Löschen des Mitarbeiters!');
+          }
+        }
+      );
   }
 }
